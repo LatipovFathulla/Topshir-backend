71a8b0d884786aa9cb42f5dd7be9a82a0150b6c5
FathullaLatipov <fathullaxonlatipov@gmail.com>
2023-05-23 18:27:00 +0500

    new blog

--- topshir/settings.py
+++ topshir/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-psjfutc9fvhq8&x1foc5hy2w#^6gy+b2ljhdah6qg0b7w-r((x'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'topshir.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#      'default': {
-#          'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#          'NAME': config('DB_NAME'),
-#          'USER': config('DB_USER'),
-#          'PASSWORD': config('DB_PASS'),
-#          'HOST': config('DB_HOST'),
-#          'PORT': config('DB_PORT'),
-#      }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+         'NAME': config('DB_NAME'),
+         'USER': config('DB_USER'),
+         'PASSWORD': config('DB_PASS'),
+         'HOST': config('DB_HOST'),
+         'PORT': config('DB_PORT'),
+     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
